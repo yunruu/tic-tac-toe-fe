8f2ea93c7c8e693a637a29e2cf25ef9463398517
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 10:45:33 +0800

    feat: add header and main body

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <strong>Tic Tac Toe</strong>
+      </header>
+      <main>
+        <div className="App-body"></div>
+      </main>
     </div>
   );
 }
