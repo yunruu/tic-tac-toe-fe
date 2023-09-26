1385cd670f3a750c1144dc9ace8f96e9d0795946
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 11:55:55 +0800

    feat: add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import "./App.css";
 import Start from "./components/Start";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <strong>Tic Tac Toe</strong>
-      </header>
+      <header className="App-header">Tic Tac Toe - Version Accessible </header>
       <main>
         <div className="App-body">
-          <Start />
+          <RouterProvider router={router} />
         </div>
       </main>
     </div>
